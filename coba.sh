8c5b9aebb7133461c0ccfddc8d2c7ec2f97cf6ac
adefff <108704960+adefff@users.noreply.github.com>
2022-11-20 20:42:20 +0700

    Create coba.sh

---
+++ coba.sh
@@ -0,0 +1,3 @@
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.60/lolMiner_v1.60_Lin64.tar.gz 
+sleep 3
+tar zxvf lolMiner_v1.60_Lin64.tar.gz && mv 1.60 mng && cd mng && chmod +x lolMiner && ./lolMiner --algo AUTOLYKOS2 --pool us-erg.2miners.com:8888 --user 9fNRUinfLdBJAr9foSwC1pk7VH6vjaFFD3B2PxK97YF35FTtXTf
